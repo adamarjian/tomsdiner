197e761008defbab003643a64b746283eda9b3c2
Agentkow <kennethkhau212@gmail.com>
2019-06-21 19:00:25 -0500

    test push

---
+++ Assets/Scripts/PointClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointClick : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Transform placement;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+
+
+    }
+
+    public void PlayerPlace()
+    {
+        Debug.Log("clicked");
+        player.transform.position = placement.position;
+    }
+}
