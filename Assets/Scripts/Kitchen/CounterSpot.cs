40bb942042be7b824ad567a1ebad7eda4e58461e
Agentkow <kennethkhau212@gmail.com>
2019-06-27 17:40:59 -0500

    player hold items alpha

---
+++ Assets/Scripts/Kitchen/CounterSpot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterSpot : MonoBehaviour
+{
+    [SerializeField]
+    private MakeFood foodHolder;
+
+    public GameObject readyItem;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (foodHolder.ready)
+        {
+            readyItem = foodHolder.food;
+            foodHolder.ready = false;
+        }
+    }
+}
