07142ac5686a523ad67163c5028175dbf069f2f1
AdrianGilbertRatuita <33073672+AdrianGilbertRatuita@users.noreply.github.com>
2019-06-27 14:26:42 -0500

    Code Changes

---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FoodTypes
+{
+
+    BREAD,
+    MILKSHAKE,
+    STEAK
+
+}
+
+[System.Serializable]
+public class Order
+{
+
+    public List<FoodTypes> FoodOrders;
+
+    public Order(int Total)
+    {
+
+        // Initialize our Food Order List
+        FoodOrders = new List<FoodTypes>();
+
+        // Make sure Total does not go beyond enum length
+        if (Total > System.Enum.GetNames(typeof(FoodTypes)).Length) Total = System.Enum.GetNames(typeof(FoodTypes)).Length;
+
+        // Continue adding a new food order until we reached the desired total
+        while (FoodOrders.Count != Total)
+        {
+
+            // Generate a random food order
+            FoodTypes NewFood = GenerateFoodOrder();
+
+            // Determine if that food order has not been added to our list
+            if (!FoodOrders.Contains(NewFood)) FoodOrders.Add(NewFood);
+
+        }
+
+    }
+
+    public FoodTypes GenerateFoodOrder()
+    {
+
+        // Create a new randomizer
+        System.Random randomizer = new System.Random();
+
+        // Always generate to the max number of enums possible
+        int randomFood = randomizer.Next(1, System.Enum.GetNames(typeof(FoodTypes)).Length);
+
+        // Return our new randomized food
+        return (FoodTypes)randomFood;
+
+    }
+
+}
---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TableState
+{
+
+    UNSEATED,
+    SEATED,
+    EATING,
+    BILLING,
+    CLEANUP
+
+}
+
+public class Table : MonoBehaviour
+{
+
+    // How long before customers order
+    // Range from Minimum to Max
+    private Vector2 olrderTimer;
+
+    // How long before customers finish eating
+    // Range from Minimum to Max
+    private Vector2 eatingTimer;
+
+    [SerializeField]
+    private Order currentOrder;
+
+    [SerializeField]
+    private Customer currentCustomer;
+
+    [SerializeField]
+    private TableState currentTableState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    float Timer;
+
+    float currentOrderTimer;
+    float currentEatingTimer;
+
+    public void StateProcessing()
+    {
+
+        switch (currentTableState)
+        {
+
+            case TableState.SEATED:
+                {
+
+                    
+                    break;
+                }
+            case TableState.EATING:
+                {
+
+                    break;
+                }
+            case TableState.BILLING:
+                {
+
+
+                    break;
+                }
+            case TableState.CLEANUP:
+                {
+
+
+                    break;
+                }
+
+        }
+
+    }
+
+    public void NewFoodOrder()
+    {
+
+
+
+    }
+
+}
